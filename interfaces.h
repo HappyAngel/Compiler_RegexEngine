ce8ad2a9c02f0bfd92000a6e1cf58f9f323ed137
HappyAngel <HappyAngel880503@gmail.com>
2015-05-14 08:45:05 +0800

    Step1 Parse Regex Expression
    1 initialy finish Step1;
    2 add unit test for step1;

--- interfaces.h
+++ interfaces.h
@@ -1,22 +1,261 @@
-__interface IOperator
+#include <stdio.h>
+#include <iostream>
+#include <stdexcept>
+#include <vector>
+using namespace std;
+
+class IOperand
 {
+public:
+
+	virtual char GetValue() = 0;
+};
+
+///
+//  this class contains base classese for the Regex engine
+//
+//
+///
+class RegexOperand : public IOperand
+{
+private:
+	char _c;
+
+public:
+	RegexOperand()
+	{
+		_c = '#';
+	}
+
+	RegexOperand (char c)
+	{
+		if (!IsValid(c))
+		{
+			throw std::invalid_argument("we only support a-z and A-Z now");
+		}
+
+		_c = c;
+	}
+
+	bool static IsValid (char c)
+	{
+		if((c >= 'a' && c <= 'z') ||
+			(c >= 'A' && c <= 'Z'))
+		{
+			return true;
+		}
+
+		return false;
+	}
+
+	virtual char GetValue()
+	{
+		return _c;
+	}
+
+	static bool CheckAndCreateOperand(char c, RegexOperand* pOperand)
+	{
+		if (IsValid(c))
+		{
+			pOperand = new RegexOperand(c);
+		}
+	}
+};
+
+class IOperator
+{
+public:
 	// get priority in number of the current operator
 	// to help compare 
-	double GetPriority();
+	virtual double GetPriority() = 0;
 
+	virtual IOperand* eval(vector<IOperand*> opVector) = 0;
 }; 
 
-__interface IOperand
+
+enum RegexOperatorType
 {
-	char GetValue();
+	UnknownRegexOperatorType=0,
+	RegexStarOperatorType,
+	RegexOrOperatorType,
+	RegexAndOperatorType,
+	RegexLeftParenthesesType,
+	RegexRightParenthesesType,
+	RegexEndType
 };
 
-__interface IUnaryOperator : IOperator
+class RegexOperator : public IOperator
 {
-	void eval(IOperand op);
+public:
+	virtual RegexOperatorType GetType() = 0;
 };
 
-__interface IBinaryOperator : IOperator
+class RegexStarOperator : public RegexOperator
 {
-	void eval(IOperand op1, IOperand op2);
+public:
+	virtual IOperand* eval(vector<IOperand*> opVector)
+	{
+		cout<<"call RegexStarOperator eval() to operate on "<<opVector[0]->GetValue()<<endl;
+		return new RegexOperand();
+	}
+
+	virtual double GetPriority()
+	{
+		return 0.9;
+	}
+
+	virtual RegexOperatorType GetType()
+	{
+		return RegexOperatorType::RegexStarOperatorType;
+	}
+};
+
+class RegexOrOperator : public RegexOperator
+{
+public:
+	virtual IOperand* eval(vector<IOperand*> opVector)
+	{
+		cout<<"call RegexOrOperator eval() on "<<opVector[0]->GetValue()<<" and "<<opVector[1]->GetValue()<<endl;
+		return new RegexOperand();
+	}
+
+	virtual double GetPriority()
+	{
+		return 0.2;
+	}
+
+	virtual RegexOperatorType GetType()
+	{
+		return RegexOperatorType::RegexOrOperatorType;
+	}
+};
+
+class RegexAndOperator : public RegexOperator
+{
+public:
+	virtual IOperand* eval(vector<IOperand*> opVector)
+	{
+		cout<<"call RegexAndOperator eval()"<<opVector[0]->GetValue()<<" and "<<opVector[1]->GetValue()<<endl;
+		return new RegexOperand();
+	}	
+
+	virtual double GetPriority()
+	{
+		return 0.5;
+	}
+
+	virtual RegexOperatorType GetType()
+	{
+		return RegexOperatorType::RegexAndOperatorType;
+	}
+};
+
+class RegexLeftParentheseOperator : public RegexOperator
+{
+public:
+	virtual IOperand* eval(vector<IOperand*> opVector)
+	{
+		cout<<"call RegexLeftParentheseOperator eval()"<<opVector.size()<<endl;
+		// do nothing
+
+		return new RegexOperand();
+	}	
+
+	virtual double GetPriority()
+	{
+		return 0;
+	}
+
+	virtual RegexOperatorType GetType()
+	{
+		return RegexOperatorType::RegexLeftParenthesesType;
+	}
+};
+
+class RegexRightParentheseOperator : public RegexOperator
+{
+public:
+	virtual IOperand* eval(vector<IOperand*> opVector)
+	{
+		cout<<"call RegexRightParentheseOperator eval()"<<opVector.size()<<endl;
+		// do nothing
+		return new RegexOperand();
+	}	
+
+	virtual double GetPriority()
+	{
+		return 0;
+	}
+
+	virtual RegexOperatorType GetType()
+	{
+		return RegexOperatorType::RegexRightParenthesesType;
+	}
+};
+
+class RegexEndOperator : public RegexOperator
+{
+public:
+	virtual IOperand* eval(vector<IOperand*> opVector)
+	{
+		cout<<"call RegexEndOperator eval()"<<opVector.size()<<endl;
+		// do nothing
+		return new RegexOperand();
+	}	
+
+	virtual double GetPriority()
+	{
+		return 0;
+	}
+
+	virtual RegexOperatorType GetType()
+	{
+		return RegexOperatorType::RegexEndType;
+	}
+};
+
+class RegexOperatorFactory
+{
+private:
+	RegexOperatorFactory()
+	{
+	}
+
+public:
+	static RegexOperator* GetRegexObject(char c)
+	{
+		RegexOperator* returnObj = NULL;
+
+		switch (c)
+		{
+		case '*':
+			returnObj =  new RegexStarOperator();
+			break;
+		case '|':
+			returnObj = new RegexOrOperator();
+			break;
+		case '&':
+			returnObj = new RegexAndOperator();
+			break;
+		case '(':
+			returnObj = new RegexLeftParentheseOperator();
+			break;
+		case ')':
+			returnObj = new RegexRightParentheseOperator();
+			break;
+		case '#':
+			returnObj = new RegexEndOperator();
+			break;
+		default:
+			return returnObj;
+		}
+
+		return returnObj;
+	}
+
+};
+
+class RegexExpression
+{
+
 };
--- main.cpp
+++ main.cpp
@@ -1,8 +1,9 @@
+#include <assert.h>
 #include <iostream>
 #include <stack>
 #include <string>
 #include <stdlib.h>
-
+#include "interfaces.h"
 
 using namespace std;
 
@@ -38,6 +39,10 @@ double GetOperatorPriority(char c)
 	{
 		return 0;
 	}
+	else
+	{
+		return 0;
+	}
 }
 
 bool isOperand(char c)
@@ -62,102 +67,238 @@ bool isOperator(char c)
 	}
 }
 
-// eval str value
-// return true on success and set outputValue
-bool eval(string str, int& outputValue)
+///preproess to parse for regex string
+// 1 add "&" for AND regex operation
+string preprocess(string str)
 {
-	stack<char> operators;
-	stack<int> operands;
-	str.append("#");
+	string processedStr;
 
-	for (int i=0; i < str.size(); i++)
+	if(str.empty())
 	{
-		if (isOperand(str[i]))
-		{
-			char c = str[i];
-			operands.push(atoi(&c));
-		}
-
-		if (isOperator(str[i]))
-		{
-			if (str[i] == '(')
-			{
-				operators.push(str[i]);
-			}
-			else if (str[i] == ')')
-			{
-				while (!operators.empty())
-				{
-					if (operators.top() == '(')
-					{
-						operators.pop();
-						break;
-					}
-					else
-					{
-						if (operands.size() > 2)
-						{
-							int op1 = operands.top();
-							operands.pop();
-							int op2 = operands.top();
-							operands.pop();
-
-							operands.push(GetValue(op1,op2, operators.top()));
-							operators.pop();
-						}
-						else
-						{
-							return false;
-						}
-					}
-				}
-			}
-			else
-			{
-				while (!operators.empty() && GetOperatorPriority(operators.top()) >= GetOperatorPriority(str[i]))
-				{
-					if (operands.size() >= 2)
-					{
-						int op1 = operands.top();
-						operands.pop();
-						int op2 = operands.top();
-						operands.pop();
-
-						operands.push(GetValue(op1,op2, operators.top()));
-						operators.pop();
-					}
-					else
-					{
-						return false;
-					}
-				}
-
-				operators.push(str[i]);
-			}
-		}
+		return processedStr;
 	}
 
-	if ((!operators.empty() && operators.top() != '#') || operands.size() != 1)
+	// step1: add operator '&'
+	int indexP = -1;
+
+	for (unsigned int i=0; i < str.size(); i++)
+	{
+		if (indexP != -1 && (
+			((RegexOperand::IsValid(str[indexP]) 
+			|| RegexOperatorFactory::GetRegexObject(str[indexP])->GetType() == RegexStarOperatorType
+			|| RegexOperatorFactory::GetRegexObject(str[indexP])->GetType() == RegexRightParenthesesType)
+						&& RegexOperand::IsValid(str[i]))
+						|| (RegexOperand::IsValid(str[indexP]) && RegexOperatorFactory::GetRegexObject(str[i])->GetType() == RegexLeftParenthesesType)))
+		{
+			processedStr += '&';
+		}
+
+		processedStr += str[i];
+		indexP = i;
+	}
+
+	//step2: add '#' as sentinel
+	processedStr += '#';
+
+	return processedStr;
+}
+
+bool evalByOperatorType(RegexOperator* pRegOperator, stack<RegexOperand*>& operands)
+{
+	if (pRegOperator == NULL || operands.empty())
 	{
 		return false;
 	}
 
-	outputValue = operands.top();
+	vector<IOperand*> pVec;
+	IOperand* pOp1 = NULL;
+	IOperand* pOp2 = NULL;
+
+	switch (pRegOperator->GetType())
+	{
+	case RegexAndOperatorType:
+	case RegexOrOperatorType:
+		{
+			if (operands.size() < 2)
+			{
+				return false;
+			}
+
+			pOp1 = operands.top();
+			operands.pop();
+			pOp2 = operands.top();
+			operands.pop();
+			pVec.push_back(pOp1);
+			pVec.push_back(pOp2);
+			operands.push((RegexOperand*)pRegOperator->eval(pVec));
+			delete pOp1;
+			delete pOp2;
+			break;
+		}
+	case RegexStarOperatorType:
+		pOp1 = operands.top();
+		operands.pop();
+		pVec.push_back(pOp1);
+		operands.push((RegexOperand*)pRegOperator->eval(pVec));
+		delete pOp1;
+		break;
+	default:
+		return false;
+	}
 
 	return true;
 }
 
+// eval str value
+// return true on success and set outputValue
+bool eval(const string str, RegexExpression* re)
+{
+	string processedStr = preprocess(str);
 
+	stack<RegexOperator*> operators;
+	stack<RegexOperand*> operands;
+
+	for (unsigned int i=0; i < processedStr.size(); i++)
+	{
+		if (RegexOperand::IsValid(processedStr[i]))
+		{
+			operands.push(new RegexOperand(processedStr[i]));
+		}
+
+		RegexOperator* pRegexOperator = NULL;
+
+		if (NULL != (pRegexOperator = RegexOperatorFactory::GetRegexObject(processedStr[i])))
+		{
+			if (pRegexOperator->GetType() == RegexLeftParenthesesType)
+			{
+				operators.push(pRegexOperator);
+			}
+			else if (pRegexOperator->GetType() == RegexRightParenthesesType)
+			{
+				RegexOperator* pTmpRegexOperator = operators.empty() ? NULL : operators.top();
+				
+				if (NULL == pTmpRegexOperator)
+				{
+					return false;
+				}
+
+				while (pTmpRegexOperator->GetType() != RegexLeftParenthesesType)
+				{
+					if (!evalByOperatorType(pTmpRegexOperator, operands))
+					{
+						return false;
+					}
+
+					operators.pop();
+					delete pTmpRegexOperator;
+					
+					if (operators.empty())
+					{
+						return false;
+					}
+					else
+					{
+						pTmpRegexOperator = operators.top();
+					}
+				}
+
+				// assert GetType() == RegexLeftParenthesesType
+				delete pTmpRegexOperator;
+				operators.pop();
+			}
+			else
+			{
+				RegexOperator* pTmpRegexOperator = NULL;
+				
+				while (!operators.empty() && (pTmpRegexOperator = operators.top())->GetPriority() >= pRegexOperator->GetPriority())
+				{
+					if (!evalByOperatorType(pTmpRegexOperator, operands))
+					{
+						return false;
+					}
+
+					operators.pop();
+					delete pTmpRegexOperator;
+				}
+
+				operators.push(pRegexOperator);
+			}
+		}
+
+	}
+
+	if ((!operators.empty() && ((RegexOperator*)operators.top())->GetType() != RegexEndType) 
+		|| operands.size() != 1)
+	{
+		return false;
+	}
+
+	return true;
+}
+
+void RegexParserUnitTest()
+{
+	// positive cases
+	string strRegex("a(b|c)*b"); 
+	RegexExpression* pRegexExp = NULL;
+	bool bSuccess1 = eval(strRegex, pRegexExp);
+	assert(bSuccess1);
+
+	string strRegex1("abc*b"); 
+	pRegexExp = NULL;
+	bSuccess1 = eval(strRegex1, pRegexExp);
+	assert(bSuccess1);
+
+	string strRegex2("a|b|c*b"); 
+	pRegexExp = NULL;
+	bSuccess1 = eval(strRegex2, pRegexExp);
+	assert(bSuccess1);
+
+	string strRegex3("a*b*c*|b"); 
+	pRegexExp = NULL;
+	bSuccess1 = eval(strRegex3, pRegexExp);
+	assert(bSuccess1);
+
+	// negative cases
+	string strRegex4("|abc*b"); 
+	pRegexExp = NULL;
+	bSuccess1 = eval(strRegex4, pRegexExp);
+	assert(!bSuccess1);
+
+	string strRegex5("*abc*b"); 
+	pRegexExp = NULL;
+	bSuccess1 = eval(strRegex5, pRegexExp);
+	assert(!bSuccess1);
+
+	string strRegex6("|a"); 
+	pRegexExp = NULL;
+	bSuccess1 = eval(strRegex6, pRegexExp);
+	assert(!bSuccess1);
+
+	string strRegex7("(a"); 
+	pRegexExp = NULL;
+	bSuccess1 = eval(strRegex7, pRegexExp);
+	assert(!bSuccess1);
+
+	string strRegex8("aa)"); 
+	pRegexExp = NULL;
+	bSuccess1 = eval(strRegex8, pRegexExp);
+	assert(!bSuccess1);
+
+	string strRegex9("a)"); 
+	pRegexExp = NULL;
+	bSuccess1 = eval(strRegex9, pRegexExp);
+	assert(!bSuccess1);
+
+	cout<<"ALL RegexParser Unit Test Passed!"<<endl;
+}
 
 int main()
 {
-	string strRegex("2*(1+2)"); 
-	int value = -1;
+	RegexParserUnitTest();
 
-	bool bSuccess1 = eval(strRegex, value);
 
-	cout<<value<<endl;
-
+	//cout<<"Convert string to Regex Parse tree done with result: "<< (bSuccess1 ? "True" : "False") <<endl;
 	int a;
 	cin>>a;
 
